c8927741d19cc2a133935458a5fc315c5cf29786
Giles Thompson <giles@papaiti.com>
2011-05-15 18:21:32 +1200

    implemented simple crop in paperclip

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,6 @@
 Huffpost::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-  Paperclip.options[:command_path] = "/usr/ImageMagick-6.6.7/bin/"
+  Paperclip.options[:command_path] = "/opt/local/bin/"
 
   # In the development environment your application's code is reloaded on
   # every request.  This slows down response time but is perfect for development
