6f2ed0ded9dde816b12161df610c2b67e8acafbf
Giles Thompson <giles@papaiti.com>
2011-06-10 18:37:12 +1200

    implemented simple keyboard navigation

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,17 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+var currentFocusInt = 0;
+
+function moveUp() {
+	currentFocusInt--;
+	if(currentFocusInt<1){ currentFocusInt = 1 }
+	$('#'+currentFocusInt).focus();
+}
+
+function moveDown() {
+	numberArticlesInt = $('.article').length;
+	currentFocusInt++;
+	if(currentFocusInt>numberArticlesInt){ currentFocusInt = numberArticlesInt }
+	$('#'+currentFocusInt).focus();
+}
