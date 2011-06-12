6b9743bcb93ed9f5d92695d5a39cc626de8f1831
Giles Thompson <giles@papaiti.com>
2011-06-12 20:16:37 +1200

    specify gem paths in attempt to fix passenger errors

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,8 @@
+if ENV['RAILS_ENV'] == 'production'  # don't bother on dev
+  ENV['GEM_PATH'] = '/home/briefly/.gems' + ':/usr/lib/ruby/gems/1.8'
+end
+
+
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
