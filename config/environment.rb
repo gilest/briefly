3cea1f89f36498f332890d44503bff1ed29610b5
Giles Thompson <giles@papaiti.com>
2011-08-17 19:55:07 +1200

    still trying to get around this annoying passenger issue

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,6 @@
 if ENV['RAILS_ENV'] == 'production'  # don't bother on dev
-  ENV['GEM_PATH'] = '/home/briefly/.gems' + ':/usr/lib/ruby/gems/1.8'
+  ENV['GEM_PATH'] = '/home/briefly/.gems' #+ ':/usr/lib/ruby/gems/1.8'  # Need this or Passenger fails to start
+  require '/home/briefly/.gems/gems/rack-1.2.2/lib/rack.rb'  # Need this for EACH LOCAL gem you want to use, otherwise it uses the ones in /usr/lib
 end
 
 
