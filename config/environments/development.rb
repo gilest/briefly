08420020ecb0743ba4e88e2c9d3b312f1d8aceba
Giles Thompson <giles@papaiti.com>
2013-07-23 23:14:42 +1200

    moving assets and getting the pipeline going again

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,6 +42,8 @@ Briefly::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   config.active_record.auto_explain_threshold_in_seconds = 0.5
 
+  config.assets.enabled = true
+
   # Do not compress assets
   config.assets.compress = false
 
