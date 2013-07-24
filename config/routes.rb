d7a61bd4b74afa2261b2945617dac4832728c724
Giles Thompson <giles@papaiti.com>
2013-07-24 17:31:05 +1200

    updated routes format

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Briefly::Application.routes.draw do
   
-  get 'admin' => 'private#index', :as => 'articles'
-  post 'admin/create' => 'private#create', :as => 'create_article'
-  get 'admin/crop/:id' => 'private#crop', :as => 'crop_article'
-  patch 'admin/update/:id' => 'private#update', :as => 'update_article'
-  get 'admin/edit/:id' => 'private#edit', :as => 'edit_article'
-  get 'admin/destroy/:id' => 'private#destroy', :as => 'destroy_article'
-  get 'admin/up/:id' => 'private#up', :as => 'up_article'
-  get 'admin/down/:id' => 'private#down', :as => 'down_article'
+  get 'admin', controller: 'private' action: 'index', as: 'articles'
+  post 'admin/create', controller: 'private', action: 'create', as: 'create_article'
+  get 'admin/crop/:id', controller: 'private', action: 'crop', as: 'crop_article'
+  patch 'admin/update/:id', controller: 'private', action: 'update', as: 'update_article'
+  get 'admin/edit/:id', controller: 'private', action: 'edit', as: 'edit_article'
+  get 'admin/destroy/:id', controller: 'private', action: 'destroy', as: 'destroy_article'
+  get 'admin/up/:id', controller: 'private', action: 'up', as: 'up_article'
+  get 'admin/down/:id', controller: 'private', action: 'down', as: 'down_article'
   
   root to: 'public#index', as: 'index'
 
