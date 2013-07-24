375bed84f324722df43c1c8969f3d29510c61a92
Giles Thompson <giles@papaiti.com>
2013-07-24 20:43:32 +1200

    rails 4 compatible updates

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Briefly::Application.config.secret_token = 'fa6c57021e5826dc3c2c210c2ef7a87a70c3fd9accc9680fe0f9b8c9f00c9a4ce8d07ab66b0f634a0f040d440305e6f8841742b3137619328defd615a3f78995'
+Briefly::Application.config.secret_key_base = 'askfjash8as4j44j4kj4l5k13njfbflasi489adfna89vnad8fgnapv8n4un8sfasefashdfjkas8aasdaiqnsiuvneisnvf94b84bfidbd84nf784bfsisx9x9zz'
