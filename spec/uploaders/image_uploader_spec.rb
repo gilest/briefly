b4333b1cb9f6924c0e5f0ab10896de9de80e2f43
Giles Thompson <giles@papaiti.com>
2014-03-30 13:51:36 +1300

    added a basic uploader spec

---
+++ spec/uploaders/image_uploader_spec.rb
@@ -0,0 +1,27 @@
+require 'carrierwave/test/matchers'
+
+describe ImageUploader do
+  include CarrierWave::Test::Matchers
+
+  before :each do
+    image_path = Rails.root.join('spec', 'fixtures', 'lolcat.jpg')
+    ImageUploader.enable_processing = true
+    @article = create :article
+    @image_uploader = ImageUploader.new(@article, :image)
+    @image_uploader.store!(File.open(image_path))
+  end
+
+  after :each do
+    ImageUploader.enable_processing = false
+    @image_uploader.remove!
+  end
+
+  it "should create a banner image at 661x240" do
+    expect(@image_uploader.banner).to have_dimensions(661, 240)
+  end
+
+  it "should make the image readable only to the owner and not executable" do
+    expect(@image_uploader).to have_permissions(0644)
+  end
+
+end
