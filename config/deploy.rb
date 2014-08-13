203665c8617479098097ec7434d0caa13c300257
Giles Thompson <giles@papaiti.com>
2014-08-12 21:20:11 -0700

    removed broken cap tasks

--- config/deploy.rb
+++ config/deploy.rb
@@ -118,20 +118,3 @@ namespace :deploy do
   end
 
 end
-
-namespace :rails do
-  desc "Remote console"
-  task :console, :roles => :app do
-    run_interactively "bundle exec rails console #{rails_env}"
-  end
-
-  desc "Remote dbconsole"
-  task :dbconsole, :roles => :app do
-    run_interactively "bundle exec rails dbconsole #{rails_env}"
-  end
-end
-
-def run_interactively(command, server=nil)
-  server ||= find_servers_for_task(current_task).first
-  exec %Q(ssh #{user}@#{server.host} -t 'sudo su - #{application} -c "cd #{current_path} && #{command}"')
-end
